69c9f67b8b0216935a5605b90ab5f7af789372f5
matilda38@naver.com <4283136na>
2017-07-06 15:40:19 +0900

    the program is working

--- src/LogAgentInGo/LogAgentInGo.go
+++ src/LogAgentInGo/LogAgentInGo.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"crypto/rand"
 	"github.com/glycerine/rbuf"
+	"fmt"
 )
 
 const (
@@ -24,6 +25,7 @@ type Config struct{
 	Connection string
 	MaxChunkSizeWan int
 	MaxChunkSizeLan int
+	Protocol string
 }
 
 type Gelf struct{
@@ -47,6 +49,9 @@ func New(config Config) *Gelf{
 	if config.MaxChunkSizeLan == 0{
 		config.MaxChunkSizeLan = defaultMaxChunkSizeLan
 	}
+	if config.Protocol == ""{
+		config.Protocol = "UDP"
+	}
 
 	rb := rbuf.NewFixedSizeRingBuf(1420000)
 	var queue bytes.Buffer
@@ -60,6 +65,21 @@ func New(config Config) *Gelf{
 }
 
 func (g *Gelf) Log(message string){
+	if g.Config.Protocol == "UDP"{
+		g.LogUDP(message)
+	} else{
+		message = fmt.Sprint(message, "\000")
+		g.SendTCP([]byte(message))
+	}
+}
+
+//When message comes
+//1. compress
+//2. if the length of compressed data is larger than the chuncksize,
+// slice it to each chunk and send each chunk.
+
+func (g *Gelf) LogUDP(message string){
+	//step 1 : compress
 	compressed := g.Compress([]byte(message))
 	chunksize := g.Config.MaxChunkSizeWan
 
@@ -74,10 +94,10 @@ func (g *Gelf) Log(message string){
 		//until length of the message
 		for i, index := 0,0; i < length; i, index = i + chunksize , index+1{
 			packet := g.CreateChunkedMessage(index, chunkCountInt, messageId, &compressed)
-			g.Send(packet.Bytes())
+			g.SendUDP(packet.Bytes())
 		}
 	} else{
-		g.Send(compressed.Bytes())
+		g.SendUDP(compressed.Bytes())
 	}
 }
 
@@ -120,7 +140,14 @@ func (g *Gelf) IntToBytes (i int) []byte {
 	return buf.Bytes()
 }
 
-func (g *Gelf) Send (b []byte){
+//For UDP Gelf
+//1. receive data
+//2. write it in ringbuffer
+//3. if the data exceeds the size of ringbuffer size(1420000),
+//	write it in normal buffer(unlimited size)
+//4. setup for connection
+//5. send data in ringbuffer
+func (g *Gelf) SendUDP (b []byte){
 	var n int
 	var err error
 
@@ -130,8 +157,8 @@ func (g *Gelf) Send (b []byte){
 		g.Buffer.Write(b[n:])
 	}
 
-	var addr = "0.0.0.0:12201"
-	udpAddr, err := net.ResolveUDPAddr("udp", addr)
+	var addrU = "0.0.0.0:12201"
+	udpAddr, err := net.ResolveUDPAddr("udp", addrU)
 	if err != nil {
 		log.Printf("%s", err)
 		return
@@ -142,4 +169,16 @@ func (g *Gelf) Send (b []byte){
 		return
 	}
 	conn.Write(g.RingBuffer.Bytes())
+}
+
+// For TCP GELF : no chunking no compressing
+func (g *Gelf) SendTCP(b []byte) {
+	var addr = "0.0.0.0:5555"
+	conn, err := net.Dial("tcp", addr)
+	if err != nil{
+		log.Print("%s", err)
+		return
+	}
+	defer conn.Close()
+	conn.Write(b)
 }
--- src/main.go
+++ src/main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"LogAgentInGo"
+	"fmt"
 )
 
 
 func main(){
 	doc := "{ \"version\" : \"1.1\", \"host\": \"example.org\", \"short_message\": \"A short message that helps you identify what is going on\", \"full_message\": \"Backtrace here\n\nmore stuff\"}"
+
+	//if there's no defined protocol, the default is udp
 	g:= LogAgentInGo.New(LogAgentInGo.Config{})
-	g.Log(doc)
+
+	for i:=0; i< 10000;i++{
+		g.Log(doc)
+	}
+
+	fmt.Print(g.Buffer)
 }
